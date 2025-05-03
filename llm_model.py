7cac8203019b47f5e9c6047d9b8bcfd9cdee1063
Sangmin Kim <112241898+Pheol9166@users.noreply.github.com>
2025-05-03 07:31:07 +0000

    05.03 / FEAT - HuggingFace에서 로컬로 모델을 가져오는 경로 추가 - 로컬 시 설정할 Config 추가 및 수정

--- llm_model.py
+++ llm_model.py
@@ -1,41 +1,33 @@
-import os
-from langchain.llms.base import LLM
-from huggingface_hub import InferenceClient
-from pydantic import Field
-
-
-class HFChatCompletionLLM(LLM):
-    model: str
-    api_key: str
-    provider: str
-    temperature: float
-    max_tokens: int
-    client: InferenceClient = Field(default=None, exclude=True)
-
-    def __init__(self, **data):
-        super().__init__(**data)
-        self.client = InferenceClient(provider=self.provider, api_key=self.api_key)
-
-    def _call(self, prompt, **kwargs):
-        completion = self.client.chat.completions.create(
-            model=self.model,
-            messages=[{"role": "user", "content": prompt}],
-            max_tokens=self.max_tokens,
-            temperature=self.temperature,
-        )
-        return completion.choices[0].message["content"]
-
-    @property
-    def _llm_type(self) -> str:
-        return "hf_chat_completion_llm"
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from langchain_huggingface.llms import HuggingFacePipeline
 
 
 def load_llm_model(config):
-    llm_params = config["llm_params"]
-    return HFChatCompletionLLM(
-        model=config["llm_model"],
-        api_key=os.getenv("HUGGINGFACEHUB_API_TOKEN"),
-        provider=llm_params.get("provider", "nebius"),
-        temperature=llm_params.get("temperature", 0.3),
-        max_tokens=llm_params.get("max_tokens", 512),
+    model_name = config['llm_model']
+    llm_params = config.get("llm_params", {})
+    
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name,
+        device_map = llm_params.get("device_map", "auto"),
+        dtype = llm_params.get("dtype", "auto")
     )
+    
+    temperature = llm_params.get("temperature", 0.3)
+    top_p = llm_params.get("top_p", 0.9 if temperature >= 0.7 else 0.95)
+    top_k = llm_params.get("top_k", 50 if temperature >= 0.7 else 20)
+    
+    hf_pipeline = pipeline(
+        "text-generation",
+        model= model,
+        tokenizer= tokenizer,
+        max_length= llm_params.get("max_tokens", 512),
+        temperature= temperature,
+        top_p= top_p,
+        top_k= top_k,
+        repetition_penalty= llm_params.get("repetition_penalty", 1.1)
+    )
+    
+    langchain_llm = HuggingFacePipeline(pipeline=hf_pipeline)
+    
+    return langchain_llm
