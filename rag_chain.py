40b3653899e47d78d63c7f48c47c2086f1e81535
Sangmin Kim <112241898+Pheol9166@users.noreply.github.com>
2025-05-04 11:22:05 +0000

    05.04 FEAT - FastAPI를 통한 비동기 응답 구성 - 책 제목이 아닌 책 ID와 이유를 같이 반환하도록 수정

--- rag_chain.py
+++ rag_chain.py
@@ -1,14 +1,31 @@
 from langchain.prompts import PromptTemplate
 from langchain_core.output_parsers import JsonOutputParser
 from operator import itemgetter
+import json
 
 
-def build_rag_chain(llm, retriever, prompt_text):
+def get_filter_id_func(book_data):
+    def filter_id(parsed_output):
+        items = parsed_output.get("recommend_books", [])
+        recommend = []
+        for item in items:
+            item_name = item.get("name").strip()
+            id = list(filter(lambda x: x["name"] == item_name, book_data))[0]["id"]
+            recommend.append({"id": id, "reason": item.get("reason", "")})
+
+        return json.dumps({"recommend_books": recommend})
+
+    return filter_id
+
+
+def build_rag_chain(llm, retriever, prompt_text, book_data):
     parser = JsonOutputParser()
 
     prompt = PromptTemplate(
         input_variables=["user_profile", "question", "context"], template=prompt_text
     )
+
+    filter_id = get_filter_id_func(book_data)
 
     rag_chain = (
         {
@@ -19,5 +36,6 @@ def build_rag_chain(llm, retriever, prompt_text):
         | prompt
         | llm
         | parser
+        | filter_id
     )
     return rag_chain
