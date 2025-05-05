f3986e1f259a4b3fb49b5a2b8f17055fc0185d66
Sangmin Kim <112241898+Pheol9166@users.noreply.github.com>
2025-05-05 06:39:00 +0000

    05.04 Chore - process_request 함수 분할     - split_request / build_documents - prompt 수정 - id filtering 함수 수정

--- process_data.py
+++ process_data.py
@@ -2,33 +2,28 @@ from langchain.schema import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 
-def split_request(json_input):
-    json_input = json_input.dict()
-    user_data = json_input["user_data"]
-    user_answer = json_input["user_data"]["user_answer"]
+def process_request(json_input, config):
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=config["splitter"].get("CHUNK_SIZE", 1000),
+        chunk_overlap=config["splitter"].get("CHUNK_OVERLAP", 200),
+    )
+    user_data = json_input["user"]
+    user_answer = json_input["user"]["answer"]
     books = json_input["books"]
 
-    return user_data, user_answer, books
-
-def build_documents(books, config):
     documents = [
         Document(
-            page_content=book["book_desc"],
+            page_content=book.book_desc,
             metadata={
-                "name": book["name"],
-                "author": book["author"],
-                "genre": ", ".join(book["gerne"]), # 장르가 List[str]로 구성되어있다 상정
-                "publisher": book["publisher"],
+                "name": book.name,
+                "author": book.author,
+                "genre": ", ".join(book.genre), # 장르가 List[str]로 구성되어있다 상정
+                "publisher": book.publisher,
             },
         )
         for book in books
     ]
-    splitter = RecursiveCharacterTextSplitter(
-    chunk_size=config["splitter"].get("CHUNK_SIZE", 1000),
-    chunk_overlap=config["splitter"].get("CHUNK_OVERLAP", 200),
-    )
-    
+
     documents = splitter.split_documents(documents)
-    
-    return documents
-    
+
+    return user_data, user_answer, documents
